4d3b4bb6eb3ea989f51ef9ce6720767478e4f093
Ann Ye <an_say52@yahoo.com>
2021-02-24 22:02:45 +0100

    Additional changes

--- auth0login/urls.py
+++ auth0login/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import include
+from django.urls import include, path
+from auth0login import views
 
 urlpatterns = [
     path('', views.index),
     path('dashboard', views.dashboard),
     path('logout', views.logout),
-    path('', include('django.contrib.auth.urls')),
-    path('', include('social_django.urls')),
 ]
